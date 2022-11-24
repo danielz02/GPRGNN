0b667f3d765856aa3a4cb50f3b7fc2c5ba89ee87
danielz <31590926+danielz02@users.noreply.github.com>
2022-11-24 15:36:59 -0600

    Print checkpoint size in static quantization

--- src/static_quant.py
+++ src/static_quant.py
@@ -9,6 +9,12 @@ from dataset_utils import DataLoader
 import torch.nn.functional as F
 
 
+def print_size_of_model(model):
+    torch.save(model.state_dict(), "/tmp/temp.p")
+    print('Size (MB):', os.path.getsize("/tmp/temp.p") / 1e6)
+    os.remove('/tmp/temp.p')
+
+
 def test(model, data, is_training=True, device="cuda:0"):
     model.eval()
     logits, accs, losses, preds = model(data.to(device)), [], [], []
@@ -35,22 +41,30 @@ def main():
     parser = ArgumentParser()
     parser.add_argument("--ckpt", type=str, required=True)
     parser.add_argument("--q-config", type=str, choices=["default", "fbgemm"])
-    parser.add_argument("--dataset", type=str)
+    parser.add_argument("--dataset", type=str, required=True)
     parser.add_argument("--device", type=str, default="cuda:0")
     args = parser.parse_args()
 
     # "../ckpt/cora_GPRGNN_8.pth"
-    model = torch.load(args.ckpt)
+    model = torch.load(args.ckpt).to(args.device)
     model.eval()
 
-    dataset, data = DataLoader("cora")
+    dataset, data = DataLoader(args.dataset)
 
-    # Nothing to fuse for GPRGNN
-    # model.fuse_model()
+    print("Performance Before Quantization")
+    print_size_of_model(model)
+    test(model, data, is_training=False, device=args.device)
+
+    # Fuse Linear - ReLU
+    torch.quantization.fuse_modules(model, ["lin1.1", "lin1.2"], inplace=True)
+
+    # Backward compatibility with old checkpoints
+    if not hasattr(model, "dequant"):
+        model = torch.ao.quantization.QuantWrapper(model)
 
     # Specify quantization configuration
-    # Start with simple min/max range estimation and per-tensor quantization of weights
-    model.qconfig = torch.ao.quantization.default_qconfig
+    # min/max range estimation and per-tensor quantization of weights
+    model.qconfig = torch.ao.quantization.get_default_qconfig(args.q_config)
     print(model.qconfig)
     torch.ao.quantization.prepare(model, inplace=True)
 
@@ -65,13 +79,12 @@ def main():
 
     # Convert to quantized model
     torch.ao.quantization.convert(model, inplace=True)
-    if not hasattr(model, "dequant"):
-        torch.ao.quantization.add_quant_dequant(model)
     print('Post Training Quantization: Convert done')
     print("Model After Quantization")
     print(model)
 
-    test(model, data, is_training=False)
+    print_size_of_model(model)
+    test(model, data, is_training=False, device=args.device)
 
 
 if __name__ == "__main__":
