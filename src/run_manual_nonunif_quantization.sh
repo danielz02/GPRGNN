75166dfa4ed5700861cb04a46ad036437b0ba97f
Bo-Rong Chen <borongc2@illinois.edu>
2022-12-06 08:38:18 +0100

    update mse

---
+++ src/run_manual_nonunif_quantization.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+python3 manual_Nonunif_quantization.py --dataset Cora
+
+python3 manual_Nonunif_quantization.py --dataset CiteSeer
+
+python3 manual_Nonunif_quantization.py --dataset PubMed
+
+python3 manual_Nonunif_quantization.py --dataset Computers
+
+python3 manual_Nonunif_quantization.py --dataset Photo
+
+python3 manual_Nonunif_quantization.py --dataset Chameleon
+
+python3 manual_Nonunif_quantization.py --dataset Squirrel
+
+python3 manual_Nonunif_quantization.py --dataset Texas
+
+python3 manual_Nonunif_quantization.py --dataset Cornell
