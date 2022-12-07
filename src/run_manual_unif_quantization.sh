f12fe797a11643b86b509bae55fb52087719a86a
Bo-Rong Chen <borongc2@illinois.edu>
2022-12-07 07:23:27 +0100

    update figures

--- src/run_manual_unif_quantization.sh
+++ src/run_manual_unif_quantization.sh
@@ -1,19 +1,96 @@
 #! /bin/sh
+#
+# This script is to reproduce our results in Table 2.
+RPMAX=100
 
-python3 manual_unif_quantization.py --dataset Cora
+# Below is for homophily datasets, sparse split
 
-python3 manual_unif_quantization.py --dataset CiteSeer
+python3 manual_unif_quantization.py --RPMAX $RPMAX \
+        --net GPRGNN \
+        --train_rate 0.025 \
+        --val_rate 0.025 \
+        --dataset Cora \
+        --lr 0.01 \
+        --alpha 0.1 
 
-python3 manual_unif_quantization.py --dataset PubMed
+python3 manual_unif_quantization.py --RPMAX $RPMAX \
+        --net GPRGNN \
+        --train_rate 0.025 \
+        --val_rate 0.025 \
+        --dataset CiteSeer \
+        --lr 0.01 \
+        --alpha 0.1
 
-python3 manual_unif_quantization.py --dataset Computers
+python3 manual_unif_quantization.py --RPMAX $RPMAX \
+        --net GPRGNN \
+        --train_rate 0.025 \
+        --val_rate 0.025 \
+        --dataset PubMed \
+        --lr 0.05 \
+        --alpha 0.2 
+        
+python3 manual_unif_quantization.py --RPMAX $RPMAX \
+        --net GPRGNN \
+        --train_rate 0.025 \
+        --val_rate 0.025 \
+        --dataset Computers \
+        --lr 0.05 \
+        --alpha 0.5 \
+        --weight_decay 0.0
+        
+python3 manual_unif_quantization.py --RPMAX $RPMAX \
+        --net GPRGNN \
+        --train_rate 0.025 \
+        --val_rate 0.025 \
+        --dataset Photo \
+        --lr 0.01 \
+        --alpha 0.5 \
+        --weight_decay 0.0
 
-python3 manual_unif_quantization.py --dataset Photo
+# Below is for heterophily datasets, dense split
 
-python3 manual_unif_quantization.py --dataset Chameleon
+python3 manual_unif_quantization.py --RPMAX $RPMAX \
+        --net GPRGNN \
+        --train_rate 0.6 \
+        --val_rate 0.2 \
+        --dataset Chameleon \
+        --lr 0.05 \
+        --alpha 1.0 \
+        --weight_decay 0.0 \
+        --dprate 0.7 
 
-python3 manual_unif_quantization.py --dataset Squirrel
+python3 manual_unif_quantization.py --RPMAX $RPMAX \
+        --net GPRGNN \
+        --train_rate 0.6 \
+        --val_rate 0.2 \
+        --dataset Film \
+        --lr 0.01 \
+        --alpha 0.9 \
+        --weight_decay 0.0 
 
-python3 manual_unif_quantization.py --dataset Texas
+python3 manual_unif_quantization.py --RPMAX $RPMAX \
+        --net GPRGNN \
+        --train_rate 0.6 \
+        --val_rate 0.2 \
+        --dataset Squirrel \
+        --lr 0.05 \
+        --alpha 0.0 \
+        --weight_decay 0.0 \
+        --dprate 0.7 
+        
+python3 manual_unif_quantization.py --RPMAX $RPMAX \
+        --net GPRGNN \
+        --train_rate 0.6 \
+        --val_rate 0.2 \
+        --dataset Texas \
+        --lr 0.05 \
+        --alpha 1.0 \
+        
+python3 manual_unif_quantization.py --RPMAX $RPMAX \
+        --net GPRGNN \
+        --train_rate 0.6 \
+        --val_rate 0.2 \
+        --dataset Cornell \
+        --lr 0.05 \
+        --alpha 0.9 
 
-python3 manual_unif_quantization.py --dataset Cornell
